598a6df263578bcc62752e9793770617cb5520ea
Andres <azourelli@gmail.com>
2019-03-27 22:28:20 -0500

    locks removed

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const db = knex({
 	}
 });
 
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000'));
 
 app.post('/NewFlights', (data, res) => {
 	console.log(data.body.flightData);
