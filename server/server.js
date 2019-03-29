4a644ade567313f8f6b9c35db3f7b07c914b6251
Andres <azourelli@gmail.com>
2019-03-28 19:26:45 -0500

    server fix maybe

--- server/server.js
+++ server/server.js
@@ -14,12 +14,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '../client/build/index.html'));
+	res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
 
 const db = knex({
